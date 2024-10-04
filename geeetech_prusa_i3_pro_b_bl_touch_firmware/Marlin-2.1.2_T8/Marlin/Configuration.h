91d90e339e4f729c3ffd2fa3d3d657ae938b1c8e
rinocs <rinocs92@gmail.com>
2024-10-04 19:15:57 +0200

    feat: added probe offset for adjustable probe

--- geeetech_prusa_i3_pro_b_bl_touch_firmware/Marlin-2.1.2_T8/Marlin/Configuration.h
+++ geeetech_prusa_i3_pro_b_bl_touch_firmware/Marlin-2.1.2_T8/Marlin/Configuration.h
@@ -1486,7 +1486,8 @@
  *     O-- FRONT --+
  */
 // #define NOZZLE_TO_PROBE_OFFSET { 9,-43, -3.47 }
-#define NOZZLE_TO_PROBE_OFFSET { 4,-44, -1.0 }
+#define NOZZLE_TO_PROBE_OFFSET { 4,-44, -1.0 } 
+// #define NOZZLE_TO_PROBE_OFFSET { -25,-41, -1.0 }
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 #define PROBING_MARGIN 30
